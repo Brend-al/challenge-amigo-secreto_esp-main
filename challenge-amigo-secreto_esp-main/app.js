df0761bd9253d60f209eca3bee0769383a43d650
Brend-al <Brendamorenovarona@gmail.com>
2025-08-08 21:51:02 -0500

    Refactor HTML structure and enhance JavaScript functions for adding and drawing friends

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,56 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema//
+let amigos = [];
+
+function agregarAmigo() {
+  const Amigos = document.getElementById("amigo");
+  const nuevoAmigo = Amigos.value.trim();
+
+  if (nuevoAmigo === "") {
+    alert("Por favor, ingrese un nombre de amigo valido");
+    return;
+  }
+
+  amigos.push(nuevoAmigo);
+  console.log(amigos);
+
+  actualizarListaAmigos();
+
+  Amigos.value = "";
+}
+
+function actualizarListaAmigos() {
+  const lista = document.getElementById("ListaAmigos");
+
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    const nombreAmigo = amigos[i];
+
+    const nuevoElementoLista = document.createElement("li");
+
+    nuevoElementoLista.textContent = nombreAmigo;
+
+    lista.appendChild(nuevoElementoLista);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length < 2) {
+    alert("¡Necesitas al menos 2 amigos para sortear!");
+    return;
+  }
+
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+  const amigoSorteado = amigos[indiceAleatorio];
+
+  const elementoResultado = document.getElementById("resultado");
+
+  if (elementoResultado) {
+    elementoResultado.innerHTML = `¡El amigo sorteado es : <strong>${amigoSorteado}</strong>!`;
+  } else {
+    console.error("No se encontro el elemento con id 'resultado'");
+  }
+}
+
+actualizarListaAmigos();
